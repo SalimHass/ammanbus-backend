956a2d04639b77bc7db5f110e819d71ce091690e
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-07-04 21:37:08 +0300

    fixing admin accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from lib2to3.pgen2.driver import Driver
 from django.contrib import admin
-from .models import User
+from .models import User,Passenger,Driver
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Passenger)
+admin.site.register(Driver)
