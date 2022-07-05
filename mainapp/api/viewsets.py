521fc0ff5688ec249d852c33eec9e21420bfea09
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-07-05 20:41:39 +0300

    fixing time

--- mainapp/api/viewsets.py
+++ mainapp/api/viewsets.py
@@ -75,7 +75,7 @@ class BusViewSet(viewsets.ModelViewSet):
         if self.action == 'list' or self.action == 'retrieve':
             permission_classes = [AllowAny]
         else:
-            permission_classes = [IsAdminUser | IsDriver]
+            permission_classes = [IsAdminUser ]
         return [permission() for permission in permission_classes]
 
 
@@ -103,7 +103,7 @@ def find_route(request, from_id, to_id):
     if to_station == from_station:
         return Response({'error': "same stations"})
     graph = build_graph()
-    paths = list(nx.shortest_simple_paths(graph, from_station.id, to_station.id, weight='time'))
+    paths = list(nx.shortest_simple_paths(graph, from_station.id, to_station.id))
     return Response({'paths': build_directions_response(graph, paths)})
 
 
@@ -115,8 +115,8 @@ def build_graph():
         station_stops = route.station_stops.all()
         for i in range(len(station_stops) - 1):
             graph.add_edges_from([(station_stops[i].id, station_stops[i + 1].id,
-                                   {"time": station_stops[i].time_to_next_station,
-                                    "distance": station_stops[i].distance_to_next_station, })])
+                                   {
+                                    "distance": station_stops[i].distance_to_next_station })])
             add_overlap_stations_edge(graph, station_stops[i], overlap_stations)
             if i == len(station_stops) - 2:
                 add_overlap_stations_edge(graph, station_stops[i + 1], overlap_stations)
@@ -129,7 +129,7 @@ def add_overlap_stations_edge(graph, station_stop, overlap_stations):
     if station_stop.station_id in overlap_stations:
         for stop_id in overlap_stations[station_stop.station_id]:
             graph.add_edges_from([(station_stop.id, stop_id,
-                                   {"time": 1, "distance": 0})])
+                                   { "distance": 0})])
 
         overlap_stations[station_stop.station_id].append(station_stop.id)
     else:
@@ -140,30 +140,30 @@ def build_directions_response(graph, paths):
     paths_json = []
     for path in paths:
 
-        total_time = nx.path_weight(graph, path, weight='time')  # Calculate total time
+        #total_time = nx.path_weight(graph, path, weight='time')  # Calculate total time
         total_distance = nx.path_weight(graph, path, weight='distance')  # Calculate distance time
-        fare = calculate_fare(total_distance)
+        #fare = calculate_fare(total_distance)
         station_stops = get_station_stops_for_path(path)
         current_route = station_stops[0].route
-        now = datetime.now() + timedelta(minutes=2)
+        #now = datetime.now() + timedelta(minutes=2)
         current_route_dic = {"route_name": current_route.name,
-                             "start_time": now.strftime("%m/%d/%Y, %H:%M:%S"), "station_stops": []}
+                              "station_stops": []}
         routes = [current_route_dic]
-        accumulate_time = 0
+        #accumulate_time = 0
         for stop in station_stops:
             pre_stop = stop
-            if pre_stop:
-                accumulate_time += pre_stop.time_to_next_station
+            """if pre_stop:
+                accumulate_time += pre_stop.time_to_next_station"""
             if current_route != stop.route:
-                start_time = now + timedelta(minutes=accumulate_time)
+                #start_time = now #+ timedelta(minutes=accumulate_time)
                 current_route = stop.route
                 current_route_dic = {"route_name": current_route.name,
-                                     "start_time": start_time.strftime("%m/%d/%Y, %H:%M:%S"), "station_stops": []}
+                                      "station_stops": []}
                 routes.append(current_route_dic)
 
             current_route_dic["station_stops"].append(StationStopSerializer(stop).data)
-        paths_json.append({"path": 1, "total_time": total_time,
-                           "distance": total_distance, "fare": fare, "routes": routes})
+        paths_json.append({"path": 1,
+                           "distance": total_distance,  "routes": routes})
     return paths_json
 
 
@@ -176,6 +176,6 @@ def get_station_stops_for_path(path):
         station_stops.append(StationStop.objects.get(id=ea[1]))
     return station_stops
 
-
+"""
 def calculate_fare(distance):
-    return FareTable.objects.get(id=1).fare + (FareTable.objects.get(id=2).fare * ceil(distance / 1000))
+    return FareTable.objects.get(id=1).fare + (FareTable.objects.get(id=2).fare * ceil(distance / 1000))"""
